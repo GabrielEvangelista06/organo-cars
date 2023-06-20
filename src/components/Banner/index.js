93cf64eac07b2297cdda20ea2cc0a3e05d5f3ff7
GabrielEvangelista06 <gabrielevangelista094@gmail.com>
2023-06-19 23:58:04 -0300

    Renomeando arquivo do componente Banner

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -3,7 +3,7 @@ import './Banner.css';
 export default function Banner() {
   return (
     <header className="banner">
-      <img src="/assets/images/banner.png" alt="O banner principal da página do Organo" />;
+      <img src="/assets/images/banner.png" alt="O banner principal da página do Organo" />
     </header>
   );
 }
