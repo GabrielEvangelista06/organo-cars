5956308c82b5318900e88b8a16fb3f1e8d7ab6c8
GabrielEvangelista06 <gabrielevangelista094@gmail.com>
2023-06-22 00:08:04 -0300

    Mostrando somente marcas que tem carros cadastrados

--- src/components/Brand/index.js
+++ src/components/Brand/index.js
@@ -6,13 +6,15 @@ export default function Brand(props) {
   const borderColor = { borderColor: props.firstColor };
 
   return (
-    <section className="brand" style={bgColor}>
-      <h3 style={borderColor}>{props.name}</h3>
-      <div className='cars'>
-        {props.cars.map(car => (
-          <Car key={car.model} model={car.model} brand={car.brand} imageUrl={car.imageUrl} releaseDate={car.releaseDate} />
-        ))}
-      </div>
-    </section>
+    props.cars.length > 0 && (
+      <section className="brand" style={bgColor}>
+        <h3 style={borderColor}>{props.name}</h3>
+        <div className="cars">
+          {props.cars.map(car => (
+            <Car key={car.model} model={car.model} brand={car.brand} imageUrl={car.imageUrl} releaseDate={car.releaseDate} />
+          ))}
+        </div>
+      </section>
+    )
   );
 }
